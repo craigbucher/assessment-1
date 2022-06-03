59854259db603e808ce7e75b48f8824b3a15bfdf
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-03 13:13:32 -0500

    currency values

--- optimal_change.py
+++ optimal_change.py
@@ -6,6 +6,23 @@
 # ensure proper grammer in output
 
 def optimal_change(cost, amount_paid):
+    #create result variable
+    change = ''
+    #map currency options to values
+    values = {
+    '$100 bill': 100,
+    '$50 bill': 50,
+    '$20 bill': 20,
+    '$10 bill': 10,
+    '$5 bill': 5,
+    '$1 bill': 1,
+    'quarter': .25,
+    'dime': .10,
+    'nickel': .5,
+    'penny': .1,
+    }
+    
+    # determine change required
     change = amount_paid - cost
 
     return(change)
