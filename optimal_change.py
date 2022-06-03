a3bffa102e5a3f261045aee06013bf272eeab1b1
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-03 13:07:39 -0500

    pseudocode

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,13 @@
 # Write your solution here!
+# calculate change required
+# determine valid bill/coin values
+# iterate from largest to smallest
+# check for edge cases
+# ensure proper grammer in output
+
+def optimal_change(cost, amount_paid):
+    change = amount_paid - cost
+
+    return(change)
+
+print(optimal_change(75.00, 100))
